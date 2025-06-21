339862408296b199fd08c8acd91d5a78136d0fab
Mathew <stcnkvld@gmail.com>
2025-06-21 16:41:40 +0300

    Изменил логику работы поиска пользователя по id. Добавил HTTPException для обработки статус-кодов. Добавил приветствие для главной страницы. Добавил uuid для аннотации типов в создании пользователя.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from fastapi import FastAPI
+from uuid import UUID
+from fastapi import FastAPI, HTTPException
 from User import User
 app = FastAPI()
 users: list[User] = []
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
 @app.get("/users")
 async def read_users():
     return {"users": users}
 
 @app.get("/users/{user_id}")
-async def read_user(user_id: int):
-    return {"user_id": user_id,
-            "name": users[user_id].name,
-            "surname": users[user_id].surname,
-            "age": users[user_id].age,
-            "email": users[user_id].email,
-            "phone_number": users[user_id].phone_number}
+async def read_user(user_id: UUID):
+    for user in users:
+        if user.id == user_id:
+            return {"user_id": user_id,
+                    "name": user.name,
+                    "surname": user.surname,
+                    "age": user.age,
+                    "email": user.email,
+                    "phone_number": user.phone_number}
+    return HTTPException(status_code=404, detail="User not found")
 
 @app.post("/users/")
 async def create_user(user: User):
