120c6fef09182918954544338e7b20be290a392f
Mathew <stcnkvld@gmail.com>
2025-06-28 23:13:17 +0300

    move get_async_session to db_helper.py

--- api_v1/users/views.py
+++ api_v1/users/views.py
@@ -3,31 +3,37 @@ from fastapi import Depends, APIRouter
 from uuid import UUID
 from api_v1.users import crud
 from sqlalchemy.ext.asyncio import AsyncSession
-from dependencies import get_async_session
+from core.models import db_helper
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.get("")
-async def read_users(db: AsyncSession = Depends(get_async_session)):
+async def read_users(db: AsyncSession = Depends(db_helper.get_async_session)):
     return await crud.read_users(db)
 
 
 @router.get("/{user_id}")
-async def read_user(user_id: UUID, db: AsyncSession = Depends(get_async_session)):
+async def read_user(
+    user_id: UUID, db: AsyncSession = Depends(db_helper.get_async_session)
+):
     return await crud.read_user(user_id, db)
 
 
 @router.post("/")
-async def create_user(user: CreateUser, db: AsyncSession = Depends(get_async_session)):
+async def create_user(
+    user: CreateUser, db: AsyncSession = Depends(db_helper.get_async_session)
+):
     return await crud.create_user(user, db)
 
 
 @router.delete("/{user_id}")
-async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_async_session)):
+async def delete_user(
+    user_id: UUID, db: AsyncSession = Depends(db_helper.get_async_session)
+):
     return await crud.delete_user(user_id, db)
 
 
 @router.delete("/")
-async def delete_users(db: AsyncSession = Depends(get_async_session)):
+async def delete_users(db: AsyncSession = Depends(db_helper.get_async_session)):
     return await crud.delete_users(db)
--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -10,6 +10,13 @@ class DatabaseHelper:
             bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
         )
 
+    async def get_async_session() -> AsyncSession:
+        async with db_helper.session_factory() as session:
+            try:
+                yield session
+            finally:
+                await session.close()
+
 
 db_helper = DatabaseHelper(
     url=settings.get_db_url(),
