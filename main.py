678abbbec2a69465bbf3e98682c1749311414a65
Mathew <stcnkvld@gmail.com>
2025-07-03 22:36:28 +0300

    return create all tables

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from api_v1.users import user_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
     yield
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(user_router)
