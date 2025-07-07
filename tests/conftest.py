e004457c2a22708e13539f5603df515aa9e4e845
Mathew <stcnkvld@gmail.com>
2025-07-07 23:54:22 +0300

    create fixture for data base

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,38 +1,23 @@
 import pytest
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.pool import StaticPool
+from core.models import Base
+import core.models
+from fastapi import FastAPI
 from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-
-from main import app
-from core.models import Base, DatabaseHelper
-
-TEST_DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5234/test_db"
+from main import app as main_app
 
 
 @pytest.fixture(scope="session")
-def asyncio_backend():
-    return "asyncio"
-
-
-@pytest.fixture(scope="function")
 async def async_client():
-    engine = create_async_engine(TEST_DATABASE_URL, echo=False)
+    engine = create_async_engine(
+        "sqlite+aiosqlite:///:memory:",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-    async def override_get_db():
-        TestingSessionLocal = sessionmaker(
-            bind=engine,
-            class_=AsyncSession,
-            expire_on_commit=False,
-        )
-        async with TestingSessionLocal() as session:
-            yield session
-
-    app.dependency_overrides[DatabaseHelper.get_async_session()] = override_get_db
-
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        yield client
-
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+    yield engine
+    await engine.dispose()
