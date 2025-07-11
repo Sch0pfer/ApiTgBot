383f250716d5f062daa84a01b4476d82c0854d42
Mathew <stcnkvld@gmail.com>
2025-07-11 12:48:35 +0300

    new migration

--- core/models/user.py
+++ core/models/user.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Mapped
 
 
 class User(Base):
-    name: Mapped[str]
-    surname: Mapped[str]
+    username: Mapped[str]
+    hashed_password: Mapped[str]
     age: Mapped[int]
     email: Mapped[str]
     phone: Mapped[str]
