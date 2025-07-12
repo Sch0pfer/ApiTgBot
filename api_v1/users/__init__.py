ad09169f22c689547b1a0fabc70f363d187f14ea
Mathew <stcnkvld@gmail.com>
2025-07-12 19:41:51 +0300

    add new router

--- api_v1/users/__init__.py
+++ api_v1/users/__init__.py
@@ -4,7 +4,9 @@ __all__ = (
     "UserUpdate",
     "UserUpdatePartial",
     "UserLogin",
+    "auth_router",
 )
 
 from .schemas import CreateUser, UserUpdate, UserUpdatePartial, UserLogin
 from .views import router as user_router
+from .auth import router as auth_router
